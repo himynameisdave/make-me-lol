18f340168fd76705bde4a176749f0eb8ea9286bb
dlunny <dlunny@featurecreeptechnology.com>
2015-11-30 16:43:08 -0500

    Initializes stuffs

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const request   = require("request");
+const open      = require("open");
+
+
+console.log('Imma make you lol dawg!');
