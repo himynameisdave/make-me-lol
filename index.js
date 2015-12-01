9d7ab57570ca54aeb116a6fa99ed93c60651643f
dlunny <dlunny@featurecreeptechnology.com>
2015-11-30 23:52:56 -0500

    v0.1.0
    Gets everything up and running!

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 #!/usr/bin/env node
+"use strict";
+const commander  = require("commander");
+const request    = require("request-promise");
+const open       = require("open");
+const redditUrls = [
+        "https://www.reddit.com/r/funnyvideos/.json",
+        "https://www.reddit.com/r/Funnypics/.json",
+        "https://www.reddit.com/r/funnygifs/.json",
+        "https://www.reddit.com/r/Funny/.json"
+      ];
+  //    Register arguments with commander
+  commander.version('0.1.0')
+            .option('-v, --video', 'opens a funny video')
+            .option('-g, --gif',   'opens a funny gif')
+            .option('-p, --pic',   'opens a funny pic')
+            .option('-q, --quiet', 'opens a funny not video')
+            .parse(process.argv);
 
-const commander = require("commander");
-const request   = require("request");
-const open      = require("open");
+  //  Sets the lookup url based on command line arguments
+  let funUrl = redditUrls[3];
+  if( commander.video ){
+    funUrl = redditUrls[0];
+  }
+  if( commander.pic ){
+    funUrl = redditUrls[1];
+  }
+  if( commander.gif ){
+    funUrl = redditUrls[2];
+  }
+  if( commander.quiet ){
+    funUrl = redditUrls[Math.floor(Math.random()*redditUrls.length)+1];
+  }
 
-
-console.log('Imma make you lol dawg!');
+  console.log('Get ready to lol...');
+  //  Go get our JSON data
+  request(funUrl)
+    .then( data => {
+      let parsed   = JSON.parse(data).data.children,
+          l = parsed.length,
+          n = Math.floor(Math.random() * (l - 1)) + 1;
+      if( commander.quiet && parsed[n].data.domain.indexOf("youtube") > -1 ){
+        n = Math.floor(Math.random() * (l - 1)) + 1;
+      };
+      //  open our funny url
+      open(parsed[n].data.url);
+      process.exit(1);
+    })
+    .catch( err => {
+      console.warn(err);
+      process.exit(0);
+    });
---
+++ readme.md
@@ -0,0 +1,39 @@
+#### make-me-lol
+
+A command-line tool to make you laugh. Because who doesn't like to laugh :smile: !
+
+**Install**
+
+```
+npm i -g make-me-lol
+```
+
+You gotta have at least `node v4.0.0` for this to work, cause it's pretty ES6exy with it's use of [Promises](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise) and all.
+
+
+**Usage**
+
+Dead simple all you gotta do is run:
+
+```
+make-me-lol [options]
+```
+
+**Options**
+
+```
+-h, --help     output usage information
+-V, --version  output the version number
+-v, --video    opens a funny video
+-g, --gif      opens a funny gif
+-p, --pic      opens a funny pic
+-q, --quiet    opens a funny not video
+```
+
+**Contribute**
+
+Feel free to open [an issue](https://github.com/himynameisdave/make-me-lol/issues/new) or [pull request](https://github.com/himynameisdave/make-me-lol/compare?expand=1)!
+
+---
+
+*Licenced under MIT (c) Made by [Dave Lunny](https://twitter.com/dave_lunny) in the beautiful year 2015*
